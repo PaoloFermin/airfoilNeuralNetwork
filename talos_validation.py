fa2a870f605c3e9e3b1329f49cc8b918fca0f0a3
Paolo Fermin <paolofermin@vt.edu>
2019-07-08 12:09:49 -0400

    fixed plotting

--- talos_validation.py
+++ talos_validation.py
@@ -1,35 +1,48 @@
 from talos import Evaluate, Restore
 import pandas as pd
-import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('TkAgg')
+import os
+from matplotlib import pyplot as plt
+
+plt.switch_backend('TkAgg')
+print(plt.get_backend())
 
 
 validation_df = pd.read_csv('optimization_validation_data.csv')
 validation_df.columns = ['case number', 'U', 'angle', 'Cd', 'Cl']
+
+validation_df.sort_values(by=['angle'], inplace=True)
 
 print(validation_df)
 
 x_val = validation_df[['U','angle']]
 y_val = validation_df[['Cd', 'Cl']]
 
-net = Restore('optimized_airfoil_nn_2.zip')
+i = 0
+while os.path.exists('optimized_airfoil_nn_%s.zip' % i):
+	i += 1
+i -= 1
+net = Restore('optimized_airfoil_nn_%s.zip'%i)
 
 pred = net.model.predict(x_val)
 print("Cd predictions: ")
 print(pred[:,0])
 print("Cl predictions: ")
 print(pred[:, 1])
-validation_df['pred_Cd'] = pred[:, 0]
-validation_df['pred_Cl'] = pred[:, 1]
+validation_df['pred_Cd'] = pred[:, 1]
+validation_df['pred_Cl'] = pred[:, 0]
 
 
 validation_df['error_Cd'] = abs((validation_df['pred_Cd'] - validation_df['Cd']) / validation_df['Cd']) * 100
 
 validation_df['error_Cl'] = abs((validation_df['pred_Cl'] - validation_df['Cl']) / validation_df['Cl']) * 100
 
-validation_df.plot(kind='scatter', x='angle', y='Cl')
-plt.show()
-validation_df.plot(kind='scatter', x='angle', y='pred_Cl')
+validation_df.plot(x='angle', y=['Cl','pred_Cl'])
+#plt.show()
+#validation_df.plot(kind='scatter', x='angle', y='pred_Cl')
 
+validation_df.plot(x='angle', y=['Cd', 'pred_Cd'])
 print(pred)
 
 print(validation_df)
